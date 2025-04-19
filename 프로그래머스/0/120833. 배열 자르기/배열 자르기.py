ce003465caf9bafcc08cef670c82f25ebe7c180b
ohhalim <ohhalim777@gmail.com>
2025-04-19 20:47:49 +0900

    [level 0] Title: 배열 자르기, Time: 0.00 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.py
@@ -0,0 +1,15 @@
+# def solution(numbers, num1, num2):
+#     answer = []
+#     for i in nunbers:
+#         if i == num1:
+#             answer += i
+#         elif:
+#             i == num2
+#             answer += i
+#         else:
+#             break
+            
+#     return answer
+
+def solution(numbers, num1, num2):
+    return numbers[num1:num2+1]
