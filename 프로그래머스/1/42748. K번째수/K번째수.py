825f6c81227523b9a86fac84036bef31cb1dade4
ohhalim <ohhalim777@gmail.com>
2025-04-22 16:03:07 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,12 +1,20 @@
+# def solution(array, commands):
+#     # 인덱스 사용한다 
+#     # sorted로 정렬한다
+#     # 몇 번째 숫자인지 찾는다
+#     # 그걸 리스트로 만들기
+#     answer = []
+#     for command in commands:
+#         i, j, k = command
+#         result = sorted(array[i-1:j])
+#         answer.append(result[k-1])
+
+#     return answer
+
 def solution(array, commands):
-    # 인덱스 사용한다 
-    # sorted로 정렬한다
-    # 몇 번째 숫자인지 찾는다
-    # 그걸 리스트로 만들기
     answer = []
     for command in commands:
         i, j, k = command
         result = sorted(array[i-1:j])
         answer.append(result[k-1])
-
     return answer
