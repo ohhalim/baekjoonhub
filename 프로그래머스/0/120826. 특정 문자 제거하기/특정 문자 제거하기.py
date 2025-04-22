38ca4cdd228d4b813f5ca2310530f5b1fb94992f
ohhalim <ohhalim777@gmail.com>
2025-04-22 12:57:43 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,3 +1,13 @@
-def solution(my_string, letter):
-    answer = my_string.replace(letter, "") # pop() 리스트에서 사용하는 매서드[] / 문자열 str 에서 안씀
+# def solution(my_string, letter):
+#     answer = my_string.replace(letter, "") # pop() 리스트에서 사용하는 매서드[] / 문자열 str 에서 안씀
+#     return answer
+
+# def solution(my_string, letter): 
+#     a = list(letter)
+#     b = list(my_string)
+#     result = map(str, b.pop(a))
+#     return b 
+
+def solution(my_string, letter): 
+    answer = my_string.replace(letter, "")
     return answer
