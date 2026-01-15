89d36fdbea44c6a12596f923eec620be7096762e
ohhalim <ohhalim777@gmail.com>
2026-01-16 08:14:03 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,11 @@
+dial = ["ABC", "DEF","GHI","JKL","MNO","PQRS", "TUV","WXZY"]
+
+word = input()
+total_time = 0
+
+for char in word:
+    for i in range(len(dial)):
+        if char in dial[i]:
+            total_time += i + 3
+            break
+print(total_time)
